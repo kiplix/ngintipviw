e0b4b23ebe559f6321605ccbc0fd1dfa4183c58b
kiplix <72283900+kiplix@users.noreply.github.com>
2021-03-15 02:06:34 +0800

    Update login.php

--- login.php
+++ login.php
@@ -13,9 +13,9 @@ echo "
 	$---------$--------$
 	
 	\n";
-echo " $$$$$$$$$$ Auto  Story Viewer by Photolooz $$$$$$$$$$$$$$$$$$   \n";
-echo "  $-----$----$ Author @nthanfp Modified by 🤑 @mohsanjid 🤑 $----$---$  \n\n";
-echo " Subscribe my Youtube channel $ PhotoLooz $ for more videos--😍
+echo " $$$$$$$$$$$$$$$$$$$$$$$$$$$   \n";
+echo "  $-----$----$ $----$---$  \n\n";
+echo " 😍
  \n\n";
 echo "[?] Input your instagram username : ";
 $userig    = trim(fgets(STDIN, 1024));
